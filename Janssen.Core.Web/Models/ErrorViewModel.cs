9c26cd184f11fa003dd6421f23e2db978a61f8b7
vezeveer <mortalmanny@live.com>
2020-05-08 18:43:52 +0800

    Add project files.

---
+++ Janssen.Core.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Janssen.Core.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
