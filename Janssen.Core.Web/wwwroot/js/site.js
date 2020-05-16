1439d40a75ae032282059e02aa4bcaf30df117bf
vezeveer <mortalmanny@live.com>
2020-05-16 12:12:39 +0800

    implemented nojavascript. Added loading screen.

--- Janssen.Core.Web/wwwroot/js/site.js
+++ Janssen.Core.Web/wwwroot/js/site.js
@@ -18,3 +18,15 @@ function toggleRequirements() {
     x.scrollIntoView({ behavior: 'smooth' });
 
 }
+
+function disableScrolling() {
+    var x = window.scrollX;
+    var y = window.scrollY;
+    window.onscroll = function () { window.scrollTo(x, y); };
+}
+
+function enableScrolling() {
+    window.onscroll = function () { };
+}
+
+disableScrolling();
